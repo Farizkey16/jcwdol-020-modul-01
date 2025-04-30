c432889fb4a6f861b3af8edd5a714508a5c06348
Farizkey16 <farizky.priambudi@gmail.com>
2025-04-30 19:11:46 +0700

    update on function, foreach and map

--- Day06/function.ts
+++ Day06/function.ts
@@ -73,3 +73,66 @@ renderCalculator(12, 5, function(nilaiA: number, nilaiB: number){
     return nilaiA/nilaiB
 }) // ==> anonymoous function
 
+// array.forEach(). array.map()
+
+/**
+ *  - .forEach() = does not transform the array, returns undefined. It's like, "for each item, perform xx action". Not building a new array, just performing an external action.
+ *  - .map() = transforms the array, return a new array, does not modify the original array
+ */
+
+// Example forEach:
+
+const prices: number[] = [4, 8, 15, 16, 23, 42] // original prices for an item
+
+prices.forEach((price, i, arr) => {
+    arr[i] = price * 0.5 // the formula forces the array to be renewed
+})
+
+console.log(prices);// this changes the original array
+
+// if we want to create a new array, we need to manually push the values to the new array
+
+const discountPrices: number[] = [];
+
+prices.forEach((price, i, arr) => {
+    discountPrices.push(price * 0.5)
+})
+
+console.log(discountPrices)
+
+// Example map:
+
+const discount_map: number[] = prices.map((price) => {
+    return price * 0.5 // map replaces the original array with returned value
+})
+console.log(prices) // original array does not modified
+console.log(discount_map) // created new array
+
+// Another example
+
+const products = [
+    {name: "Laptop", price: 499, color: "White"},
+    {name: "Smartphone", price: 899, color: "Black"},
+    {name: "Headphones", price: 50, color: "White"},
+    {name: "Tablet", price: 199, color: "Grey"},
+    {name: "Keyboard", price: 210, color: "Blue"}
+]
+
+const discounts = products.map((product) => {
+    // return product.price * 0.5 // this is just to overwrite ONLY the price
+    return {
+        ...product,
+        price: product.price * 0.5
+    }})
+
+console.log(discounts)
+
+const categorizedProducts = products.map(product => {
+    let category: string;
+    if (product.price < 100) category = "Budget";
+    else if (product.price < 500) category = "Mid-range";
+    else category = "Premium";
+    return {...product, category}
+})
+
+console.log(categorizedProducts)
